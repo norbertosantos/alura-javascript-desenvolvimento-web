cb2e0ce260cda431b40be156c82e2e747d7f4c80
Norberto da Silva Santos Filho - DATAPREVRJ <norberto.filho@dataprev.gov.br>
2017-10-26 16:45:37 -0200

    Inclusão do arquivo principal.js com o script para pegar os valores do cálculo do IMC dos pacientes

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,2 +1,35 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var tdAltura = paciente.querySelector(".info-altura");
+
+var peso = tdPeso.textContent;
+var altura = tdAltura.textContent;
+var tdImc = paciente.querySelector(".info-imc");
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if (peso <= 0 || peso >= 1000) {
+	
+	console.log("Peso Inválido");
+	tdImc.textContent ="Peso Inválido!";
+	pesoEhValido = false;
+}
+
+if (altura <= 0 || altura >= 3.00) {
+	
+	console.log("Altura Inválida!");
+	tdImc.textContent = "Altura Inválida";
+	alturaEhValida = false;
+}
+
+if (pesoEhValido && alturaEhValida) {
+	
+	var imc = peso / (altura * altura);
+	tdImc.textContent = imc;	
+}
+
