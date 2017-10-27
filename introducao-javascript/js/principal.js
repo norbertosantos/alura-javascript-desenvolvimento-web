74a79cdbbff7054061ec287edb03f26159dc6ae6
Norberto da Silva Santos Filho - DATAPREVRJ <norberto.filho@dataprev.gov.br>
2017-10-27 14:32:33 -0200

    Alteração para o cálculo do imc de todos os pacientes

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,35 +1,42 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+//var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var tdAltura = paciente.querySelector(".info-altura");
+for (var i=0; i < pacientes.length; i++) {
+	var paciente = pacientes[i];
 
-var peso = tdPeso.textContent;
-var altura = tdAltura.textContent;
-var tdImc = paciente.querySelector(".info-imc");
+	var tdPeso = paciente.querySelector(".info-peso");
+	var tdAltura = paciente.querySelector(".info-altura");
 
-var pesoEhValido = true;
-var alturaEhValida = true;
+	var peso = tdPeso.textContent;
+	var altura = tdAltura.textContent;
+	var tdImc = paciente.querySelector(".info-imc");
 
-if (peso <= 0 || peso >= 1000) {
+	var pesoEhValido = true;
+	var alturaEhValida = true;
+
+	if (peso <= 0 || peso >= 1000) {
 	
-	console.log("Peso Inválido");
-	tdImc.textContent ="Peso Inválido!";
-	pesoEhValido = false;
+		console.log("Peso Inválido");
+		tdImc.textContent ="Peso Inválido!";
+		pesoEhValido = false;
+	}
+
+	if (altura <= 0 || altura >= 3.00) {
+	
+		console.log("Altura Inválida!");
+		tdImc.textContent = "Altura Inválida";
+		alturaEhValida = false;
+	}
+
+	if (pesoEhValido && alturaEhValida) {
+	
+		var imc = peso / (altura * altura);
+		tdImc.textContent = imc.toFixed(2);	
+	}	
 }
 
-if (altura <= 0 || altura >= 3.00) {
-	
-	console.log("Altura Inválida!");
-	tdImc.textContent = "Altura Inválida";
-	alturaEhValida = false;
-}
 
-if (pesoEhValido && alturaEhValida) {
-	
-	var imc = peso / (altura * altura);
-	tdImc.textContent = imc;	
-}
 
